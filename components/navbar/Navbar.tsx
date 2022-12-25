11f388ea6f8b4f09ffbaeb23c25eff521a16e5b8
jmurphy5613 <jmurphy5613@gmail.com>
2022-12-24 19:28:55 -0500

    finished login frontend

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import styles from './Navbar.module.css'
+import LoginLogout from './loginlogout/LoginLogout';
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.name}>Jordan: Sign in and Out</h1>
             <div className={styles["align-right"]}>
-                <button>hey</button>
+                <LoginLogout />
             </div>
         </div>
     );
