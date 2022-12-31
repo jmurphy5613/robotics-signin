3442858c2083620941bc4eb071027265c8e1f851
jmurphy5613 <jmurphy5613@gmail.com>
2022-12-31 11:57:35 -0500

    sync

--- components/user-components/event-card/EventCard.tsx
+++ components/user-components/event-card/EventCard.tsx
@@ -8,11 +8,7 @@ const EventCard = () => {
                 1) Presenting designs from Thursday. Make sure you have
                 something to present. If you or your group can’t make it, we
                 will try and do a video call on discord so you can see the other
-                designs.{" "}
-            </h3>
-            <h3 className={styles.description}>
-                2) Won't be one continuous session from 10AM-2PM and we won’t
-                have a meeting Monday.
+                designs.
             </h3>
             <div className={styles["button-group"]}>
                 <button className={styles.going}>I'll be there</button>
