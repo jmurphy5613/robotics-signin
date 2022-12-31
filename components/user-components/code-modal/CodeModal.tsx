4f559180f60ff6fa24f1b7c3aed67aedd3c8eb1f
jmurphy5613 <jmurphy5613@gmail.com>
2022-12-31 12:19:58 -0500

    code modal done

--- components/user-components/code-modal/CodeModal.tsx
+++ components/user-components/code-modal/CodeModal.tsx
@@ -1,16 +1,18 @@
 import styles from './CodeModal.module.css'
 import { BsCodeSlash } from 'react-icons/bs'
 import { IconContext } from 'react-icons';
+import { AiOutlineQuestionCircle } from 'react-icons/ai'
 
 const CodeModal = () => {
     return (
         <div className={styles.container}>
             <div className={styles["modal-container"]}>
-                <IconContext.Provider value={{ color: "white", size: "50px" }}>
-                    <BsCodeSlash />
+                <IconContext.Provider value={{ color: "white", size: "60px" }}>
+                    <AiOutlineQuestionCircle />
                 </IconContext.Provider>
-                <h3 className={styles.title}>Enter the code given at the meeting</h3>
+                <h3 className={styles.title}>What's the code given at the metting?</h3>
                 <input className={styles["code-input"]} placeholder='got the code?' />
+                <button className={styles.submit}>Lets go!</button>
             </div>
         </div>
     );
