eb4a75d4c2d73af754e4ff131721d34f40964e6c
jmurphy5613 <jmurphy5613@gmail.com>
2023-02-07 12:12:22 -0500

    hours stuff

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { getAllUsers } from "../utils/requests/users"
 import styles from '../styles/Leaderboard.module.css'
-import { User } from "../utils/types"
+import { Event, User } from "../utils/types"
 
 
 const Leaderboard = () => {
@@ -13,9 +13,27 @@ const Leaderboard = () => {
         users.sort((element1, element2) => {
             if(element1.eventsAttended.length < element2.eventsAttended.length) return 1
             else return -1
-            return 0
         })
         setUsers(users)
+    }
+
+    const getTotalNumberOfHours = (users: Array<User>) => {
+        let hours = 0;
+        for(const user of users) {
+            hours += getNumberOfHours(user.eventsAttended)
+
+        }
+        return hours
+    }
+
+    const getNumberOfHours = (events: Array<Event>) => {
+        let hours = 0;
+        for(const event of events) {
+            let start = new Date(event.startDate)
+            let end = new Date(event.endDate)
+            hours += end.getHours()-start.getHours()
+        }
+        return hours
     }
 
     useEffect(() => {
@@ -24,12 +42,14 @@ const Leaderboard = () => {
 
     return (
         <div className={styles.container}>
+            <h3 className={styles["total-hours"]}>Total Hours Spent at Robotics: {getTotalNumberOfHours(users)}</h3>
             <div className={styles["grid-header"]}>
                 <h3 className={styles.title}>Robotics Member</h3>
                 <h3 className={styles.title}>Meetings Attended</h3>
             </div>
             <div className={styles.divider} />
             {users.map((element:User, index:number) => {
+                const hours = getNumberOfHours(element.eventsAttended)
                 return (
                     <>
                         <div className={styles.member}>
@@ -39,6 +59,7 @@ const Leaderboard = () => {
                             </h2>
                             <h2 className={styles.number}>
                                 {element.eventsAttended.length}
+                                ({hours} hours)
                             </h2>
                         </div>
                     </>
